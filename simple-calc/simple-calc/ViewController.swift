a19fa255eef1ecc15864f482328a701e993be545
aaronluannguyen <aaronluannguyen@gmail.com>
2018-10-16 14:02:24 -0700

    Initialized simple-calc-iOS projecet

---
+++ simple-calc/simple-calc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  simple-calc
+//
+//  Created by Aaron Nguyen on 10/16/18.
+//  Copyright © 2018 Aaron Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
